d07872a052dfdf3ccfb4c9278954c30898daad7c
Aaron Santo <42657503+Lil-Strudel@users.noreply.github.com>
2024-01-03 10:09:34 -0700

    feat: svelte implimentation

---
+++ src/components/Click.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let count = 0;
+
+  function handleClick() {
+    count = count + 1;
+  }
+</script>
+
+<div class="flex flex-col justify-center">
+  <h1 class="text-xl">{count}</h1>
+  <button
+    on:click={handleClick}
+    class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+  >
+    Click Me
+  </button>
+</div>
