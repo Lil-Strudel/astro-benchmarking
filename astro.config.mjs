19703a43c83e9125725db076ff140c0d40c2ce79
Aaron Santo <42657503+Lil-Strudel@users.noreply.github.com>
2024-01-02 22:13:01 -0700

    feat: added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,16 @@ import alpinejs from "@astrojs/alpinejs";
 import preact from "@astrojs/preact";
 import solidJs from "@astrojs/solid-js";
 import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), preact(), solidJs(), svelte()],
+  integrations: [
+    tailwind(),
+    alpinejs(),
+    preact(),
+    solidJs(),
+    svelte(),
+    react(),
+  ],
 });
