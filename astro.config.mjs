fb5b42e8f3e29e76792d26e77d6e9622d3996fe2
Aaron Santo <42657503+Lil-Strudel@users.noreply.github.com>
2024-01-03 10:12:09 -0700

    feat: added solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
 import preact from "@astrojs/preact";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), preact()],
+  integrations: [tailwind(), alpinejs(), preact(), solidJs()],
 });
-
